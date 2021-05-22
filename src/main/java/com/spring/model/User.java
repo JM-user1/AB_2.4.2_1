8009d552d08b889153dfc1be837d10e5c2040c35
JM-user1 <80413442+JM-user1@users.noreply.github.com>
2021-05-22 22:32:56 +0700

    end1

--- src/main/java/com/spring/model/User.java
+++ src/main/java/com/spring/model/User.java
@@ -12,6 +12,7 @@ import java.util.Set;
 @Table(name = "users")
 public class User implements UserDetails {
 
+    //new Branch2
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
